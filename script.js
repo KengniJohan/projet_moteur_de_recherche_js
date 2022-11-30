1eb5bba4b7983cd3df81c793bde1433b3e629f9d
KengniJohan <107089322+KengniJohan@users.noreply.github.com>
2022-11-30 12:35:01 +0100

    Modif

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
+// document.querySelectorAll('.link-items a')
+// .forEach(link => {
+//     link.addEventListener("click", function(){
+//         $(".active").className = ""
+//         link.classList.add("active")
+//     })
+// })
+
 $("#form").submit(function (e){
     e.preventDefault()
-    tndbapi
+
     var query = $("#search").val()
 
-    let ACCESS_KEY = "f71c81e37f795d4385b28233486f55a8"
+    let ACCESS_KEY = "8bc05b83d5a698c902daf8bcb5a51d40906a9fcfa11425fb3b3310070cae8d66"
 
     let result
     
-    let url = "http://api.serpstack.com/search?access_key=" + ACCESS_KEY +
-              "&type=videos&query=" + query
+    let url = "https://serpapi.com/search.json?engine=google&q=" + query + ""
     
     $.get(url, function(data){
-        console.log(data)
+        // console.log(data)
 
-        data.organic_results.forEach(res => {
+        data.search_information.menu_items.forEach(res => {
             result = `
 
                 <h1>${res.title}<\h1><br><a href = "${res.url}" target = "_blank">${res.url}</a>
@@ -24,3 +31,4 @@ $("#form").submit(function (e){
         });
     })
 })
+
